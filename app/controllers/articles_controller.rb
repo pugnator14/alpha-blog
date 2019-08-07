47e375d363993d66918735bac6c0c7213b79e9ae
pugnator14 <vladmurgoci@gmail.com>
2019-08-07 10:39:31 +0300

    Errors create and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,12 +3,17 @@ class ArticlesController <ApplicationController
     @article = Article.new
   end
   def create
-    #render plain: params[:article]
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was sucessfully created!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
-
+  def show
+    @article = Article.find(params[:id])
+  end
   private
   def article_params
     params.require(:article).permit(:title, :desc)
