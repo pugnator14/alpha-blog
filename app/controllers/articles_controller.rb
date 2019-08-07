0b5ad92b2dff76c50fa78c75969e5615da149f3b
pugnator14 <vladmurgoci@gmail.com>
2019-08-07 11:41:46 +0300

    Optimization

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,27 @@ class ArticlesController <ApplicationController
   def new
     @article = Article.new
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def delete
+    @article = Article.find(params[:id])
+    @article.delete
+    Article.update!
+    redirect_to article_path(@article)
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was sucessfully updated!"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  def index
+    @articles = Article.all
+  end
   def create
     @article = Article.new(article_params)
     if @article.save
